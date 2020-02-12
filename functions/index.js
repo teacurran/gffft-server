26a71f7bca33638451682881027331fa7aa1a9a5
Terrence Curran <tcurran@approachingpi.com>
2020-02-12 18:15:17 -0500

    reversed the string for nouns so it uses the singular

--- functions/index.js
+++ functions/index.js
@@ -13,67 +13,31 @@ const firestore = new Firestore({
 
 exports.addNouns = functions.https.onRequest(async (req, res) => {
   console.log('addNouns()');
+  let writes = [];
   if (req.method === 'DELETE') throw new Error('not yet built');
   if (req.method === 'POST') {
-    // store/insert a new document
-    const data = req.rawBody.toString();
-
-    let lines = data.split('\n');
+    let lines = req.rawBody.toString().split('\n');
     lines.forEach((line) => {
-      console.log(`line: ${line}`);
-      addToCollection(COLLECTION_NOUNS, line);
+      writes.push(addToCollection(COLLECTION_NOUNS, line.split(' ').reverse()));
     });
-
-    return res.status(200).send("done?");
-
-    // const ttl = Number.parseInt(data.ttl);
-    // const ciphertext = (data.ciphertext || '').replace(/[^a-zA-Z0-9-]*/g, '');
-    // const created = new Date().getTime();
-    // return firestore.collection(COLLECTION_NAME)
-    //   .add({created, ttl, ciphertext})
-    //   .then(doc => {
-    //     return res.status(200).send(doc);
-    //   }).catch(err => {
-    //     console.error(err);
-    //     return res.status(404).send({error: 'unable to store', err});
-    //   });
+    await Promise.all(writes).then(() => {
+      return res.status(200).send("promise I'm done");
+    });
   }
-  // read/retrieve an existing document by id
-  // if (!(req.query && req.query.id)) {
-  //   return res.status(404).send({error: 'No Id'});
-  // }
-  // const id = req.query.id.replace(/[^a-zA-Z0-9]/g, '').trim();
-  // if (!(id && id.length)) {
-  //   return res.status(404).send({error: 'Empty Id'});
-  // }
-  // return firestore.collection(COLLECTION_NAME)
-  //   .doc(id)
-  //   .get()
-  //   .then(doc => {
-  //     if (!(doc && doc.exists)) {
-  //       return res.status(404).send({error: 'Unable to find the document'});
-  //     }
-  //     const data = doc.data();
-  //     return res.status(200).send(data);
-  //   }).catch(err => {
-  //     console.error(err);
-  //     return res.status(404).send({error: 'Unable to retrieve the document'});
-  //   });
 });
 
 exports.addVerbs = functions.https.onRequest(async (req, res) => {
+  let writes = [];
   if (req.method === 'DELETE') throw new Error('not yet built');
   if (req.method === 'POST') {
-    // store/insert a new document
-    const data = req.rawBody.toString();
-
-    let lines = data.split('\n');
+    let lines = req.rawBody.toString().split('\n');
     lines.forEach((line) => {
-      addToCollection(COLLECTION_VERBS, line);
+      writes.push(addToCollection(COLLECTION_VERBS, line));
     });
-
-    return res.status(200).send("done??");
   }
+  await Promise.all(writes).then(() => {
+    return res.status(200).send("promise I'm done");
+  });
 });
 
 exports.addAdjectives = functions.https.onRequest(async (req, res) => {
@@ -81,12 +45,7 @@ exports.addAdjectives = functions.https.onRequest(async (req, res) => {
   let writes = [];
   if (req.method === 'DELETE') throw new Error('not yet built');
   if (req.method === 'POST') {
-    // store/insert a new document
-    const data = req.rawBody.toString();
-
-    let lines = data.split('\n');
-    console.log(`processing ${lines.length}`);
-    const batch = firestore.batch();
+    let lines = req.rawBody.toString().split('\n');
     lines.forEach((line) => {
       writes.push(addToCollection(COLLECTION_ADJECTIVES, line));
     });
@@ -97,14 +56,14 @@ exports.addAdjectives = functions.https.onRequest(async (req, res) => {
 });
 
 const addToCollection = async (collection, value) => {
-  // if (!value) {
-  //   return false;
-  // }
+  if (!value) {
+    return Promise.resolve('no value');
+  }
   console.log(`line: ${value}`);
-  let line_split = value.split(' ');
-  // if (line_split.length <= 0) {
-  //   return false;
-  // }
+  let line_split = Array.isArray(value) ? value : value.split(' ');
+  if (line_split.length <= 0) {
+    return Promise.resolve('no value');
+  }
   let word = line_split[0];
   console.log(`word: ${word}`);
   if (!word.includes('_')) {
