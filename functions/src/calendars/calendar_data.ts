b0209f111bbfdd79893bf04111cf45aa3218fd23
Terrence Curran <tea@approachingpi.com>
2022-01-09 11:40:26 -0500

    calendar wasn't returning an id

--- functions/src/calendars/calendar_data.ts
+++ functions/src/calendars/calendar_data.ts
@@ -1,10 +1,11 @@
-import {query, subcollection, where, limit, add, pathToRef, get} from "typesaurus"
+import {query, subcollection, where, limit, add, pathToRef, get, upset} from "typesaurus"
 import {Calendar} from "./calendar_models"
 import {User} from "../users/user_models"
 import {Gffft} from "../gfffts/gffft_models"
 import {gffftsCollection} from "../gfffts/gffft_data"
 
-const DEFAULT_BOARD_KEY = "default"
+const DEFAULT_CALENDAR_KEY = "default"
+const DEFAULT_CALENDAR_NAME = "calendar"
 
 export const calendarCollection = subcollection<Calendar, Gffft, User>("calendars", gffftsCollection)
 
@@ -18,20 +19,27 @@ export async function getOrCreateDefaultCalendar(userId: string, gffftId: string
   const userGalleries = calendarCollection([userId, gffftId])
 
   let calendar = await query(userGalleries, [
-    where("key", "==", DEFAULT_BOARD_KEY),
+    where("key", "==", DEFAULT_CALENDAR_KEY),
     limit(1),
-  ]).then((results) => {
+  ]).then(async (results) => {
     if (results.length > 0) {
-      const value = results[0].data
-      value.id = results[0].ref.id
-      return value
+      const item = results[0].data
+      item.id = results[0].ref.id
+
+      // upgrading old data
+      if (!item.name) {
+        item.name = DEFAULT_CALENDAR_NAME
+        await upset<Calendar>(results[0].ref, item)
+      }
+
+      return item
     }
     return null
   })
 
   if (calendar == null) {
     calendar = {
-      key: DEFAULT_BOARD_KEY,
+      key: DEFAULT_CALENDAR_KEY,
     } as Calendar
     const result = await add<Calendar>(userGalleries, calendar)
     calendar.id = result.id
@@ -41,9 +49,19 @@ export async function getOrCreateDefaultCalendar(userId: string, gffftId: string
 }
 
 export async function getCalendarByRef(refId: string): Promise<Calendar | null> {
-  return get(pathToRef<Calendar>(refId)).then((snapshot) => {
+  const itemRef = pathToRef<Calendar>(refId)
+  return get(itemRef).then(async (snapshot) => {
     if (snapshot != null) {
-      return snapshot.data
+      const item = snapshot.data
+      item.id = snapshot.ref.id
+
+      // upgrading old data
+      if (!item.name) {
+        item.name = DEFAULT_CALENDAR_NAME
+        await upset<Calendar>(itemRef, item)
+      }
+
+      return item
     }
     return null
   })
